1874e5cdda8ab05e6e47d9937dc42dfd7e27a7dc
pjshu <shushugo233@gmail.com>
2020-06-26 17:43:22 +0800

    foo

--- README.md
+++
@@ -1,2 +0,0 @@
-Data Structures and Algorithms in Python 
-- metaclass orm
---
+++ leetcode/array/21.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    def removeDuplicates(self, nums) -> int:
+        max_length = len(nums)
+        length = 0
+        offset = 0
+        for index, item in enumerate(nums):
+            if max_length > (index + 1) and item == nums[index + 1]:
+                continue
+            nums[length] = item
+            length += 1
+        return length
+
+
+nums = [0, 0, 0, 1, 1, 1, 4, 5]
+s = Solution()
+length = s.removeDuplicates(nums)
+for i in range(length):
+    print(nums[i])
